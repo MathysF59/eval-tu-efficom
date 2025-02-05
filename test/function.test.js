a3f5f902cbdda865ee8ea37c5e055b0c47ef7c79
MathysF59 <franchomme.mathys@outlook.fr>
2025-02-05 08:52:02 +0100

    feat: test calculateTotalPrice

--- test/function.test.js
+++ test/function.test.js
@@ -21,5 +21,47 @@ describe('When I check if a number is even or odd', () => {
     });
     test('if the number is an integer, negative, odd', () =>{
         expect(isEven(-7)).toEqual(false);
-    });    
+    });
+});
+
+describe('When I calculate the total price of a cart, with tax added', () => {
+    test('if the cart is not an array, then I should get an error', () => {
+        try{
+            let result = calculateTotalPrice(4, 0.2);
+        }
+        catch(e){
+            expect(e).not.toBeNull(); 
+            expect(e.message).toBe("Prices must be an array");
+        }
+    });
+    test('if the tax rate is not a number, then I should get an error', () => {
+        try{
+            let result = calculateTotalPrice([4, 5], "t");
+        }
+        catch(e){
+            expect(e).not.toBeNull(); 
+            expect(e.message).toBe("Tax rate must be a number");
+        }
+    });
+    test('if the price is not a number, then I should get an error', () => {
+        try{
+            let result = calculateTotalPrice([4, "t"], 0.2);
+        }
+        catch(e){
+            expect(e).not.toBeNull(); 
+            expect(e.message).toBe("Each price must be a non-negative number");
+        }
+    });
+    test('if the price is negative, then I should get an error', () => {
+        try{
+            let result = calculateTotalPrice([4, -5], 0.2);
+        }
+        catch(e){
+            expect(e).not.toBeNull(); 
+            expect(e.message).toBe("Each price must be a non-negative number");
+        }
+    });
+    test('if the price is an integer, positive', () => {
+        expect(calculateTotalPrice([4, 5], 0.2)).toEqual(10.8);
+    });
 });
