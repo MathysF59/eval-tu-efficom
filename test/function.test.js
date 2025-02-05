cad918fecbba6fe058fd72a1a98dacc74583a795
Clement <clem@gitea.dev>
2025-02-05 10:04:22 +0100

    merge main into tests

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,4 @@
 const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
-
 describe('When I check if a number is even or odd', () => {
     test('if the parameter is not a number, then I should get an error', () => {
         try{
@@ -21,13 +20,13 @@ describe('When I check if a number is even or odd', () => {
     });
     test('if the number is an integer, negative, odd', () =>{
         expect(isEven(-7)).toEqual(false);
-    });    
+    });
 });
 
-describe('When I calculate the total price of a cart', () => {
+describe('When I calculate the total price of a cart, with tax added', () => {
     test('if the cart is not an array, then I should get an error', () => {
         try{
-            let result = calculateTotalPrice(5, 0.2);
+            let result = calculateTotalPrice(4, 0.2);
         }
         catch(e){
             expect(e).not.toBeNull();
@@ -36,7 +35,7 @@ describe('When I calculate the total price of a cart', () => {
     });
     test('if the tax rate is not a number, then I should get an error', () => {
         try{
-            let result = calculateTotalPrice([5, 10], "t");
+            let result = calculateTotalPrice([4, 5], "t");
         }
         catch(e){
             expect(e).not.toBeNull();
@@ -45,7 +44,7 @@ describe('When I calculate the total price of a cart', () => {
     });
     test('if the price is not a number, then I should get an error', () => {
         try{
-            let result = calculateTotalPrice([5, "t"], 0.2);
+            let result = calculateTotalPrice([4, "t"], 0.2);
         }
         catch(e){
             expect(e).not.toBeNull();
@@ -54,19 +53,33 @@ describe('When I calculate the total price of a cart', () => {
     });
     test('if the price is negative, then I should get an error', () => {
         try{
-            let result = calculateTotalPrice([5, -10], 0.2);
+            let result = calculateTotalPrice([4, -5], 0.2);
         }
         catch(e){
             expect(e).not.toBeNull();
             expect(e.message).toBe("Each price must be a non-negative number");
         }
     });
-    test('if the cart is an array of prices and the tax rate is a number', () => {
-        expect(calculateTotalPrice([5, 10], 0.2)).toEqual(18);
+    test('if the price is an integer, positive', () => {
+        expect(calculateTotalPrice([4, 5], 1.4)).toEqual(21.6);
     });
 });
 
-describe('When I process the purchase of a cart', () => {
+describe('When I send a notification in the console', () => {
+    test('if I send a notification "Hello" in the console, then I should see "Notification envoyée : Hello" in the console', () => {
+        console.log = jest.fn();
+        sendNotification("Hello");
+        expect(console.log).toHaveBeenCalledWith("Notification envoyée : Hello");
+    });
+});
+
+describe('When I process purchase of cart after calculating total price and notificate it in the console', () => {
+    test('if the cart is a array of positive prices and the taxRate is postive, then I should get the total price and a notification in the console', () => {
+        console.log = jest.fn();
+        expect(processPurchase([4, 5], 1.4)).toEqual(21.6);
+        expect(console.log).toHaveBeenCalledWith("Notification envoyée : Votre total est de 21.60 €");
+    });
+
     test('if the cart is not an array, then I should get an error', () => {
         try{
             let result = processPurchase(5, 0.2);
@@ -103,21 +116,22 @@ describe('When I process the purchase of a cart', () => {
             expect(e.message).toBe("Each price must be a non-negative number");
         }
     });
-    test('if the cart is an array of prices and the tax rate is a number', () => {
-        expect(processPurchase([5, 10], 0.2)).toEqual(18);
-    });
 });
 
-describe('When I send a notification', () => {
-    test('if the message is a string, then I should get a notification', () => {
-        expect(sendNotification("Hello")).toBe("Notification envoyée : Hello");
-    });
-});
-
-describe('When I generate a password', () => {
+describe('When I generate a random password based on length and complexity required', () => {
     test('if the length is not a number, then I should get an error', () => {
         try{
-            let result = generatePassword("t", { uppercase: true, numbers: true, specialChars: true });
+            let result = generatePassword("t", {uppercase: true, numbers: false, specialChars: true});
+        }
+        catch(e){
+            expect(e).not.toBeNull();
+            expect(e.message).toBe("Length must be a number greater than or equal to 6");
+        }
+    });
+
+    test('if the length is less than 6, then I should get an error', () => {
+        try{
+            let result = generatePassword(4, {uppercase: true, numbers: false, specialChars: true});
         }
         catch(e){
             expect(e).not.toBeNull();
@@ -133,7 +147,20 @@ describe('When I generate a password', () => {
             expect(e.message).toBe("Options must be an object");
         }
     });
+    test('if all the options are disabled, then I should get an error', () => {
+        try{
+            let result = generatePassword(6, {uppercase: false, numbers: false, specialChars: false});
+        }
+        catch(e){
+            expect(e).not.toBeNull();
+            expect(e.message).toBe("At least one character type must be enabled");
+        }
+    });
     test('if the length is a number and the options are an object', () => {
         expect(generatePassword(8, { uppercase: true, numbers: true, specialChars: true })).not.toBeNull();
+    });
+    test('if I generate a password with a lenght equal or superior to 6, with at least one option activated, then I should get a password', () => {
+        let result = generatePassword(41, {uppercase: true, numbers: false, specialChars: true});
+        expect(result).not.toBeNull();
     });
 });
