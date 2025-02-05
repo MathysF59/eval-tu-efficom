15b8ad72f7a63f5f2f0256f78d7291b8ac738513
MathysF59 <franchomme.mathys@outlook.fr>
2025-02-05 08:42:18 +0100

    feat: test isEven

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,25 @@
 const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
 
-test("",() =>{
-
+describe('When I check if a number is even or odd', () => {
+    test('if the parameter is not a number, then I should get an error', () => {
+        try{
+            let result = isEven("t");
+        }
+        catch(e){
+            expect(e).not.toBeNull(); 
+            expect(e.message).toBe("Input must be a number");
+        }
+    });
+    test('if the number is an integer, positive and even', () => {
+        expect(isEven(4)).toEqual(true);
+    });
+    test('if the number is an integer, positive, odd', ()=> {
+        expect(isEven(7)).toEqual(false);
+    });
+    test('if the number is an integer, negative, even', () =>{
+        expect(isEven(-4)).toEqual(true);
+    });
+    test('if the number is an integer, negative, odd', () =>{
+        expect(isEven(-7)).toEqual(false);
+    });    
 });
